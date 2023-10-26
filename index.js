b02073f38ea40e60d23f97f9da58d105572de4fa
andrew <andrew_hoffman262@outlook.com>
2023-10-25 18:27:50 -0600

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const titleCase = (str) => {
+  return str.split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ')
+}
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase);
 }
